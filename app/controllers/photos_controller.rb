984e7c9da358917616aa908d9098f694b6700efd
rhys <rbrooker105@gmail.com>
2016-10-29 10:41:20 +1100

    display user individual user photo stream

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,13 @@ class PhotosController < ApplicationController
 			render :partial => '/photos/unavailable'
 		end
 	end
+
+	def show
+		begin
+			@photos = user_photos(params[:user_id])
+		rescue
+	  	render :partial => '/photos/unavailable'
+		end
+	end
+
 end
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -2,18 +2,29 @@ module PhotosHelper
 
 	def user_photos(user_id, photo_count = 40)
     photos = flickr.photos.search(:user_id => user_id, :per_page => photo_count)
+    flickr_stream(photos)
   end
   
   def recent_flickr(photo_count = 40)
     photos = flickr.photos.getRecent(:per_page => photo_count)
-    flickr_photos(photos)
+    flickr_stream(photos)
   end
 
+  def user_stream(photos, user_id, columns = 6, pic_size = "q")
+    puts user_id
+    photos = photos.map { |x|  { title: x.title,
+        pic: "https://farm#{x.farm}.staticflickr.com/#{x.server}/#{user_id}_#{x.secret}_#{pic_size}.jpg", 
+        link: "https://www.flickr.com/photos/#{x.owner}/#{user_id}" } }
+    #photos = photos.in_groups_of(columns)
+  end
 
-  def flickr_photos(photos, columns = 8, pic_size = "q")
-    photos = photos.map { |x| { title: x.title,
-      pic: "https://farm#{x.farm}.staticflickr.com/#{x.server}/#{x.id}_#{x.secret}_#{pic_size}.jpg", 
-      link: "https://www.flickr.com/photos/#{x.owner}/#{x.id}" } }
+  def flickr_stream(photos, columns = 8, pic_size = "q")
+    photos = photos.map { |x|  { title: x.title,
+        pic: "https://farm#{x.farm}.staticflickr.com/#{x.server}/#{x.id}_#{x.secret}_#{pic_size}.jpg", 
+        link: "search?utf8=✓&user_id=#{x.owner}&commit=Search",
+        #link: "https://www.flickr.com/photos/#{x.owner}/#{x.id}",
+        user: "#{x.owner}" } }
+
     photos = photos.in_groups_of(columns)
   end
 
@@ -39,3 +50,4 @@ module PhotosHelper
   #end
 
 end
+   # photos = photos.map { |x|  { title: x.title, pic: "https://farm#{x.farm}.staticflickr.com/#{x.server}/#{x.id}_#{x.secret}_#{pic_size}.jpg", link: "https://www.flickr.com/photos/#{x.owner}/#{x.id}", user: x.owner } }
